dd21834da711247a7e2a0cefd9aa1083219008ff
keithgabriel <khojhogabby@gmail.com>
2024-02-27 12:19:48 -0800

    a simple code

---
+++ simple_code.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * simple_code  - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void simple_code(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
